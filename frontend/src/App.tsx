c13fa673c6c97ecf5a05249d521e8d1e2f263c8a
francivaldolima <francivaldolimadesousa@gmail.com>
2022-07-11 10:15:54 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
-function App() {
+import NotificationButton from "./components/NotificationButton";
 
-  return(
-    <h1>Olá mundo</h1>
-  )
+function App() {
+  return (
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButton/>
+    </>
+  );
 }
 
 export default App;

0745109899c5b409f3cbbc6c11d6b61a7604edc1
francivaldolima <francivaldolimadesousa@gmail.com>
2022-07-11 10:56:37 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import logo from '../../assets/img/logo.svg';
+
+import './styles.css';
+
+function Header() {
+  return (
+    <header>
+      <div className="dsmeta-logo-container">
+        <img src={logo} alt="DSMeta" />
+        <h1>DSMeta</h1>
+        <p>
+          Desenvolvido por
+          <a href="https://github.com/francivaldolima" target="_blank">
+            github.com/francivaldolima
+          </a>
+        </p>
+        <p>
+          Desenvolvido por
+          <a href="https://www.linkedin.com/in/francivaldolima" target="_blank">
+            linkedin.com/in/francivaldolima
+          </a>
+        </p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
